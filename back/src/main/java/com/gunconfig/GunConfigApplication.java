5cda558ad5e388c5072844befb961f971c9c4dea
abvl <vladislav.abrosimov@ananas.rs>
2022-10-12 14:20:21 +0200

    main:init

---
+++ back/src/main/java/com/gunconfig/GunConfigApplication.java
@@ -0,0 +1,11 @@
+package com.gunconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GunConfigApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(GunConfigApplication.class);
+  }
+}
