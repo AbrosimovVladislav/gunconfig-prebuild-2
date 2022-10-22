708492155573ecee1acca19969db67fe8e406749
strahinjacokicananas <strahinja.cokic@ananas.rs>
2022-10-22 20:55:53 +0200

    Changed favicon

--- front/src/components/header/header.tsx
+++ front/src/components/header/header.tsx
@@ -7,6 +7,7 @@ const useStyles = createStyles((theme) => ({
     header: {
         paddingLeft: theme.spacing.md,
         paddingRight: theme.spacing.md,
+        marginBottom: "0px !important",
     },
 
     inner: {
