f508d32077f795a3c208ed0de5cde639427d64b4
abvl <vladislav.abrosimov@ananas.rs>
2022-10-24 11:13:23 +0200

    main: deploy draft flow

--- start.sh
+++ start.sh
@@ -1,5 +1,16 @@
+git checkout main
+git pull
+
 mvn clean package -P build-docker-image
 
 docker-compose down
 docker-compose pull
 docker-compose up -d
+
+pm2 kill
+
+cd ..
+cd front
+npm i
+npm run build
+pm2 serve build 3000 --spa
