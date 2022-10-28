56ea87ad5adaca5c318c7320668d94d1d2abcc98
abvl <vladislav.abrosimov@ananas.rs>
2022-10-28 10:25:12 +0200

    react-query: review fixes

--- front/src/pages/nft-catalog.tsx
+++ front/src/pages/nft-catalog.tsx
@@ -1,27 +1,26 @@
 import React from "react";
-import { useQuery } from "react-query";
-import { getAllNfts } from "../services/nftService";
-import { NFTMicroCard } from "../components/nft-micro-card";
-import { NFTCardDto } from "../schema/NFTCatalogSchema";
+import {useGetAllNfts} from "../services/nftService";
+import {NFTMicroCard} from "../components/nft-micro-card";
+import {NFTCardDto} from "../schema/NFTCatalogSchema";
 
 const NFTCatalog = () => {
-    const { data, error, isLoading, isError } = useQuery("getAllNfts", getAllNfts);
+  const [data, error, isLoading, isError] = useGetAllNfts();
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
-    if (isError) {
-        return <div>Error</div>;
-    }
+  if (isError) {
+    return <div>Error</div>;
+  }
 
-    return (
-        <div>
-            {data.map((item: NFTCardDto) => (
-                <NFTMicroCard item={item} key={item.id} />
-            ))}
-        </div>
-    );
+  return (
+      <div>
+        {data.map((item: NFTCardDto) => (
+            <NFTMicroCard item={item} key={item.id}/>
+        ))}
+      </div>
+  );
 };
 
 export default NFTCatalog;
