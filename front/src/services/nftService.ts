6209d9954cf048f819bc7db6f876b441ab896a25
abvl <vladislav.abrosimov@ananas.rs>
2022-10-28 16:28:08 +0200

    main: review fixes

--- front/src/services/nftService.ts
+++ front/src/services/nftService.ts
@@ -1,4 +1,11 @@
-export const getAllNfts = async () => {
+import {useQuery} from "react-query";
+
+export function useGetAllNfts() {
+  const {data, error, isLoading, isError} = useQuery("getAllNfts", getAllNfts);
+  return [data, error, isLoading, isError];
+}
+
+const getAllNfts = async () => {
   const response = await fetch(`http://178.128.203.194:8080/api/v1/nft-catalog/`);
   return response.json();
 };
