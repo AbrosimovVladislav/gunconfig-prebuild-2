167c56ac26381eb63debc2313d07eeb4e31a1751
anastasiiakuzennaia <nastya.kuzennaya@gmail.com>
2022-11-09 10:49:42 +0100

    microcard-hover: added hover

--- front/src/components/catalog/CatalogStyles.ts
+++ front/src/components/catalog/CatalogStyles.ts
@@ -27,5 +27,9 @@ export const useStyles = createStyles((theme) => ({
             marginLeft: "10px",
             marginRight: "10px",
         },
+        '&:hover': {
+            outline: "1px solid #7B61FF",
+            borderRadius: "8px",
+        }
     },
 }));
