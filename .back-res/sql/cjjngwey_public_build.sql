89024a27807546c8c5237ed296ecb02482cf561c
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-11-11 14:54:10 +0100

    separating: data fixes

---
+++ .back-res/sql/cjjngwey_public_build.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.build (build_id, build_image_url, schema) VALUES (1, 'qwe', '{"id":1,"children":[{"id":2,"children":[]},{"id":3,"children":[{"id":4,"children":[]}]}]}');
+INSERT INTO public.build (build_id, build_image_url, schema) VALUES (2, 'qwe', '{"id":1,"children":[{"id":5,"children":[]},{"id":3,"children":[{"id":4,"children":[]}]}]}');
+INSERT INTO public.build (build_id, build_image_url, schema) VALUES (3, 'qwe', '{"id":1,"children":[{"id":6,"children":[]},{"id":3,"children":[{"id":4,"children":[]}]}]}');
--- .back-res/sql/old/dcj30eh6ino2oj_public_build_1.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO public.build (build_id, build_image_url, element_id) VALUES (1, '1', 1);
