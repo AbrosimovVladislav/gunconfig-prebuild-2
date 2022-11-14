d603bcbfe292715eec229efa3e4cc296f8be1d84
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-11-14 15:18:57 +0100

    test-test: some fixes

--- front/src/pages/nft/[id].tsx
+++ front/src/pages/nft/[id].tsx
@@ -1,10 +1,16 @@
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useGetNFTById } from "../../services/nftService";
 
 type Props = {};
 
 const SingleNFTPage = (props: Props) => {
     const id = useRouter().query.id;
+    const [data, error, isLoading, isError] = useGetNFTById(id);
+
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
 
     return <div>NFT id: {id}</div>;
 };
--- front/src/services/nftService.ts
+++ front/src/services/nftService.ts
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { get } from "./restClient";
 import { NFT_POSTFIX } from "../consts/back-paths";
 
-export function useGetNFTById(id: number) {
+export function useGetNFTById(id: string | string[]) {
     const { data, error, isLoading, isError } = useQuery("GetNFTById:" + id, () => get(NFT_POSTFIX + id));
     return [data, error, isLoading, isError];
 }
