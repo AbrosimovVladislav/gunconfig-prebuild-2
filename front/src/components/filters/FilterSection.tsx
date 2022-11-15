5b5e30a5ebdf22c1522683cbc5f2e4e508b469fd
anastasiiakuzennaia <nastya.kuzennaya@gmail.com>
2022-11-15 09:41:03 +0100

    Setting components of filterblock, RangeFilter and CheckboxFilter: added components

--- front/src/components/filters/FilterSection.tsx
+++ front/src/components/filters/FilterSection.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { Filter, FilterType, getAllFilters } from "../../services/filterService";
+import { FilterType, getAllFilters } from "../../services/filterService";
 import { useStyles } from "./FilterSectionStyles";
+import { RangeFilter as RangeFilterType } from "../../services/filterService";
+import { CheckboxFilter as CheckboxFilterType } from "../../services/filterService";
 import RangeFilter from "./range-filter/RangeFilter";
 import CheckboxFilter from "./checkbox-filter/CheckboxFilter";
 
 const FilterSection = () => {
     const filters = getAllFilters();
-    const { classes } = useStyles();
+    const {classes} = useStyles();
     console.log(filters);
     return <div className={classes.section}>
         {filters.map((item) => (
             <div className={classes.section}>
-                {item.type == FilterType.RANGE ? <RangeFilter filter={item as Filter}/> : <CheckboxFilter filter={item as Filter}/>}
+                {item.type == FilterType.RANGE ? <RangeFilter filter={item as RangeFilterType}/> :
+                    <CheckboxFilter filter={item as CheckboxFilterType}/>}
             </div>
         ))}
 
