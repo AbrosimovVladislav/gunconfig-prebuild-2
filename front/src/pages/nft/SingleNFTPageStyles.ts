8faeb575dc02dff8e68aadbb78d91a27a6478203
logovas1994@gmail.com <simfskilletonikos2258>
2022-11-16 17:54:59 +0300

    3383bv2: review fixes

--- front/src/pages/nft/SingleNFTPageStyles.ts
+++ front/src/pages/nft/SingleNFTPageStyles.ts
@@ -2,8 +2,6 @@ import { createStyles } from "@mantine/core";
 
 export const useStyles = createStyles((theme) => ({
     grid: {
-        padding: `0 ${theme.spacing.xl} 0 ${theme.spacing.xl}`,
-        // paddingLeft: theme.spacing.xl,
-        // paddingRight: theme.spacing.xl
+        padding: [`0 ${theme.spacing.xl}px 0 ${theme.spacing.xl}px`]
     },
 }));
