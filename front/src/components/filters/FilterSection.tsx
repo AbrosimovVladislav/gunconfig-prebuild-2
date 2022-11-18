d99dbef13dd164b00cc4deeb3da51a1b524813da
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-11-18 20:00:48 +0100

    33831ct: removing children props and setting him as default for GCAccordion.tsx

--- front/src/components/filters/FilterSection.tsx
+++ front/src/components/filters/FilterSection.tsx
@@ -8,19 +8,26 @@ import { GCAccordion } from "../../gc-components/accordion/GCAccordion";
 
 const FilterSection = () => {
     const [data, isLoading, isError, isSuccess] = useGetFilters();
-    const {classes} = useStyles();
+    const { classes } = useStyles();
     if (isSuccess) {
-        return <div className={classes.section}>
-            {data.map((filterItem) => (
-                <GCAccordion className={classes.filter}
-                             children={<></>}
-                             showName={filterItem.showName}
-                             panel={filterItem.filterType == FilterType.RANGE ?
-                                 <RangeFilter filter={filterItem}/> :
-                                 <CheckboxFilter filter={filterItem}/>} />
-            ))}
-        </div>;
+        return (
+            <div className={classes.section}>
+                {data.map((filterItem) => (
+                    <GCAccordion
+                        className={classes.filter}
+                        showName={filterItem.showName}
+                        panel={
+                            filterItem.filterType == FilterType.RANGE ? (
+                                <RangeFilter filter={filterItem} />
+                            ) : (
+                                <CheckboxFilter filter={filterItem} />
+                            )
+                        }
+                    />
+                ))}
+            </div>
+        );
     }
-}
+};
 
 export default FilterSection;
--- front/src/gc-components/accordion/GCAccordion.tsx
+++ front/src/gc-components/accordion/GCAccordion.tsx
@@ -11,23 +11,33 @@ interface GCAccordionProps extends AccordionProps {
     panel: ReactNode;
 }
 
-export const GCAccordion = ({className, children, showName, panel, size, ...props}: GCAccordionProps) => {
+export const GCAccordion = ({ className, children, showName, panel, size, ...props }: GCAccordionProps) => {
     const [value, setValue] = useState<string[]>([]);
-    const {classes} = useStyles();
-    return <Accordion multiple value={value} className={classes.filter} onChange={setValue} {...props}
-                      chevron={<IconPlus size={16}/>}
-                      styles={{
-                          chevron: {
-                              '&[data-rotate]': {
-                                  transform: 'rotate(45deg)',
-                              },
-                          },
-                      }}>
-        <Accordion.Item unstyled className={classes.filterItem} value={showName}>
-            <Accordion.Control>{showName}</Accordion.Control>
-            <Accordion.Panel>
-                {panel}
-            </Accordion.Panel>
-        </Accordion.Item>
-    </Accordion>;
+    const { classes } = useStyles();
+    return (
+        <Accordion
+            multiple
+            value={value}
+            className={classes.filter}
+            onChange={setValue}
+            {...props}
+            chevron={<IconPlus size={16} />}
+            styles={{
+                chevron: {
+                    "&[data-rotate]": {
+                        transform: "rotate(45deg)",
+                    },
+                },
+            }}
+        >
+            <Accordion.Item unstyled className={classes.filterItem} value={showName}>
+                <Accordion.Control>{showName}</Accordion.Control>
+                <Accordion.Panel>{panel}</Accordion.Panel>
+            </Accordion.Item>
+        </Accordion>
+    );
+};
+
+GCAccordion.defaultProps = {
+    children: <></>,
 };
