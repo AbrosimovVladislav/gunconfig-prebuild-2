446b8b77cde88b1e8d556b002a28298b840b26f0
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-11-19 15:09:49 +0100

    3383bw3: review fixes

--- front/src/components/nft-card-information/NftCardInformationStyle.ts
+++ front/src/components/nft-card-information/NftCardInformationStyle.ts
@@ -1,15 +1,15 @@
 import { createStyles } from "@mantine/core";
 
 export const useStyles = createStyles((theme) => ({
-    listSize: {
-        margin: [`20px 0 ${theme.spacing.xl}px`],
-    },
+  listSize: {
+    margin: [`${theme.spacing.lg}px 0 ${theme.spacing.lg}px`],
+  },
 
-    priceText: {
-        marginTop: "10px",
-    },
+  priceText: {
+    marginTop: "10px",
+  },
 
-    textDescription: {
-        marginBottom: "10px",
-    },
+  textDescription: {
+    marginBottom: "10px",
+  },
 }));
