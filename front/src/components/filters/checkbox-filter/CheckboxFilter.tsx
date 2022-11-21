413e974886796ff5fc020e8765822992f44797f2
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-11-21 20:32:45 +0100

    31mqzkz: draft filtration logic

--- front/src/components/filters/checkbox-filter/CheckboxFilter.tsx
+++ front/src/components/filters/checkbox-filter/CheckboxFilter.tsx
@@ -7,62 +7,76 @@ import { useRouter } from "next/router";
 import { createFilterPostfix } from "../../../services/nftService";
 
 interface CheckboxFilterProps {
-    filter: FilterItem;
+  filter: FilterItem;
 }
 
 const CheckboxFilter = ({ filter }: CheckboxFilterProps) => {
-    const { classes } = useStyles();
-    const { filters, addFilterItem, removeFilterItem, addFilterValue, removeFilterValue } = useFilterStore();
-    const router = useRouter();
+  const { classes } = useStyles();
+  const {
+    filters,
+    addFilterItem,
+    removeFilterItem,
+    addFilterValue,
+    removeFilterValue,
+  } = useFilterStore();
+  const router = useRouter();
 
-    function clickOnFilterValue(filterName: string, value: string) {
-        const filterItem: FilterItem = filters.filter((e) => e.showName === filter.showName)[0];
+  function clickOnFilterValue(filterName: string, value: string) {
+    const filterItem: FilterItem = filters.filter(
+      (e) => e.showName === filter.showName
+    )[0];
 
-        console.log("FILTERS", filters);
+    // console.log("FILTERS BEFORE", filters);
 
-        if (filterItem) {
-            //when filterItem already exists
-            if (filterItem.value.includes(value)) {
-                //when value already in the list
-                if (filterItem.value.length == 1) {
-                    //when value in the list is the last
-                    removeFilterItem(filter.showName);
-                } else {
-                    //when value in the list is not last
-                    removeFilterValue(filter.showName, value);
-                }
-            } else {
-                //when value is not in the list
-                addFilterValue(filter.showName, value);
-            }
+    if (filterItem) {
+      //when filterItem already exists
+      if (filterItem.value.includes(value)) {
+        //when value already in the list
+        if (filterItem.value.length == 1) {
+          //when value in the list is the last
+          removeFilterItem(filter.showName);
         } else {
-            //when filterItem not exists
-            addFilterItem(filter.showName, value, filter.filterType, filter.filterKey);
+          //when value in the list is not last
+          removeFilterValue(filter.showName, value);
         }
-
-        const postfix = createFilterPostfix(filters);
-
-        console.log("POSTFIX", postfix);
-
-        router.push(postfix ? `/nft-catalog?${postfix}` : `/nft-catalog`);
+      } else {
+        //when value is not in the list
+        addFilterValue(filter.showName, value);
+      }
+    } else {
+      //when filterItem not exists
+      addFilterItem(
+        filter.showName,
+        value,
+        filter.filterType,
+        filter.filterKey
+      );
     }
 
-    return (
-        <div className={classes.filter}>
-            {filter.value.map((value) => {
-                return (
-                    <GCCheckbox
-                        onClick={() => clickOnFilterValue(filter.showName, value)}
-                        key={value}
-                        className={classes.filter}
-                        label={value}
-                    >
-                        {value}
-                    </GCCheckbox>
-                );
-            })}
-        </div>
-    );
+    const postfix = createFilterPostfix(filters);
+
+    // console.log("FILTERS AFTER", filters);
+    // console.log("POSTFIX", postfix);
+
+    router.push(postfix ? `/nft-catalog?${postfix}` : `/nft-catalog`);
+  }
+
+  return (
+    <div className={classes.filter}>
+      {filter.value.map((value) => {
+        return (
+          <GCCheckbox
+            onClick={() => clickOnFilterValue(filter.showName, value)}
+            key={value}
+            className={classes.filter}
+            label={value}
+          >
+            {value}
+          </GCCheckbox>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CheckboxFilter;
--- front/src/store/FilterStore.ts
+++ front/src/store/FilterStore.ts
@@ -2,79 +2,106 @@ import create from "zustand";
 import { FilterItem, FilterType } from "../schema/FilterSchema";
 
 interface FilterStoreState {
-    filters: FilterItem[];
-    addFilterItem: (filterName: string, value: string, filterType: FilterType, filterKey: string) => void;
-    removeFilterItem: (filterName: string) => void;
-    addFilterValue: (filterName: string, value: string) => void;
-    removeFilterValue: (filterName: string, value: string) => void;
-}
-
-export const useFilterStore = create<FilterStoreState>((set) => ({
-    filters: [],
-    addFilterItem: (filterName: string, value: string, filterType: FilterType, filterKey: string) => {
-        set((state) => ({
-            filters: addFilterItem(state.filters, filterName, value, filterType, filterKey),
-        }));
-    },
-    removeFilterItem: (filterName: string) => {
-        set((state) => ({
-            filters: removeFilterItem(state.filters, filterName),
-        }));
-    },
-    addFilterValue: (filterName: string, value: string) => {
-        set((state) => ({
-            filters: addFilterValue(state.filters, filterName, value),
-        }));
-    },
-    removeFilterValue: (filterName: string, value: string) => {
-        set((state) => ({
-            filters: removeFilterValue(state.filters, filterName, value),
-        }));
-    },
-}));
-
-function addFilterItem(
-    filters: FilterItem[],
+  filters: FilterItem[];
+  addFilterItem: (
     filterName: string,
     value: string,
     filterType: FilterType,
     filterKey: string
+  ) => void;
+  removeFilterItem: (filterName: string) => void;
+  addFilterValue: (filterName: string, value: string) => void;
+  removeFilterValue: (filterName: string, value: string) => void;
+}
+
+export const useFilterStore = create<FilterStoreState>((set) => ({
+  filters: [],
+  addFilterItem: (
+    filterName: string,
+    value: string,
+    filterType: FilterType,
+    filterKey: string
+  ) => {
+    set((state) => ({
+      filters: addFilterItem(
+        state.filters,
+        filterName,
+        value,
+        filterType,
+        filterKey
+      ),
+    }));
+  },
+  removeFilterItem: (filterName: string) => {
+    set((state) => ({
+      filters: removeFilterItem(state.filters, filterName),
+    }));
+  },
+  addFilterValue: (filterName: string, value: string) => {
+    set((state) => ({
+      filters: addFilterValue(state.filters, filterName, value),
+    }));
+  },
+  removeFilterValue: (filterName: string, value: string) => {
+    set((state) => ({
+      filters: removeFilterValue(state.filters, filterName, value),
+    }));
+  },
+}));
+
+function addFilterItem(
+  filters: FilterItem[],
+  filterName: string,
+  value: string,
+  filterType: FilterType,
+  filterKey: string
 ): FilterItem[] {
-    let newFilterItem: FilterItem = {
-        showName: filterName,
-        value: [value],
-        filterType: filterType,
-        filterKey: filterKey,
-    };
-    filters.push(newFilterItem);
+  let newFilterItem: FilterItem = {
+    showName: filterName,
+    value: [value],
+    filterType: filterType,
+    filterKey: filterKey,
+  };
+  filters.push(newFilterItem);
 
-    // console.log(filterName, value);
-    // console.log(filters);
-    return filters;
+  // console.log(filterName, value);
+  // console.log(filters);
+  return filters;
 }
 
-function removeFilterItem(filters: FilterItem[], filterName: string): FilterItem[] {
-    const updatedFilters = filters.filter((e) => e.showName !== filterName);
-
-    // console.log(filterName);
-    // console.log(filters);
-    return updatedFilters;
+function removeFilterItem(
+  filters: FilterItem[],
+  filterName: string
+): FilterItem[] {
+  console.log("BEFORE FILTERS", filters);
+  const updatedFilters = filters.filter((e) => e.showName !== filterName);
+  console.log("FILTER NAME", filterName);
+  console.log("AFTER FILTERS", updatedFilters);
+  return updatedFilters;
 }
 
-function addFilterValue(filters: FilterItem[], filterName: string, value: string): FilterItem[] {
-    filters.filter((e) => e.showName === filterName)[0].value.push(value);
+function addFilterValue(
+  filters: FilterItem[],
+  filterName: string,
+  value: string
+): FilterItem[] {
+  filters.filter((e) => e.showName === filterName)[0].value.push(value);
 
-    // console.log(filterName, value);
-    // console.log(filters);
-    return filters;
+  // console.log(filterName, value);
+  // console.log(filters);
+  return filters;
 }
 
-function removeFilterValue(filters: FilterItem[], filterName: string, value: string): FilterItem[] {
-    const currentFilters = filters.filter((e) => e.showName === filterName)[0];
-    const updatedValues = currentFilters.value.filter((e) => e !== value);
-    filters.filter((e) => e.showName === filterName)[0].value = updatedValues;
+function removeFilterValue(
+  filters: FilterItem[],
+  filterName: string,
+  value: string
+): FilterItem[] {
+  const currentFilters = filters.filter((e) => e.showName === filterName)[0];
+  const updatedValues = currentFilters.value.filter((e) => e !== value);
+  filters.filter((e) => e.showName === filterName)[0].value = updatedValues;
 
-    // console.log(filterName, value);
-    // console.log(filters);
-    return filters;
+  // console.log(filterName, value);
+  // console.log(filters);
+  return filters;
 }
