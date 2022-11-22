c6dfc08124d96e30073a58dfdadba3ea5e4aa59f
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-11-22 19:42:52 +0100

    filter-logic-6: merge try

--- front/src/pages/nft-catalog/index.tsx
+++ front/src/pages/nft-catalog/index.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import { useStyles } from "./NFTCatalogStyles";
 import FilterSection from "../../components/filters/FilterSection";
 import NFTCatalogWrapper from "../../components/nft-catalog-wrapper/NFTCatalogWrapper";
 import GCContainer from "../../gc-components/GCContainer";
+import GCSegmentedControl from "../../gc-components/segmented-control/GCSegmentedControl";
 
 const NFTCatalog = () => {
   const { classes } = useStyles();
+
+    const [layout, setLayout] = useState('grid-9');
 
   return (
     <GCContainer className={classes.page}>
       <FilterSection />
       <NFTCatalogWrapper />
+        <GCSegmentedControl value={layout} onChange={setLayout}/>
     </GCContainer>
   );
 };
