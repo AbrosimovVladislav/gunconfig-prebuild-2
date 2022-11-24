4d115b9bd8a3c977197ffd66b61a3883211fedcd
abvl <vladislav.abrosimov@ananas.rs>
2022-11-24 11:01:23 +0100

    big_daddy: review fixes

--- front/src/gc-components/GCContainer.tsx
+++ front/src/gc-components/GCContainer.tsx
@@ -1,17 +1,21 @@
 import { Container } from "@mantine/core";
 import React, { ReactNode } from "react";
+import { GCAccordion } from "./accordion/GCAccordion";
 
 type Props = {
     children: ReactNode;
     className?: any;
 };
 
-function GCContainer({ children, className }: Props) {
+export default function GCContainer({ children, className }: Props) {
     return (
-        <Container size="xl" className={className ?? {}}>
+        <Container size="xl" className={className}>
             {children}
         </Container>
     );
 }
 
-export default GCContainer;
+GCContainer.defaultProps = {
+    className: {}
+};
+
