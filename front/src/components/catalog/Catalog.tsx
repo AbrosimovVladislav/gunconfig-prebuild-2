b5aeb3f0fd5fc2e05009a374a038d2e301778ce1
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-11-27 11:20:19 +0100

    big_daddy: merge try

--- front/src/components/catalog/Catalog.tsx
+++ front/src/components/catalog/Catalog.tsx
@@ -1,9 +1,7 @@
-import { NFTCard } from "../../schema/NFTCatalogSchema";
+import {NFTCard} from "../../schema/NFTCatalogSchema";
 import React from "react";
 import NFTMicroCard from "../nft-micro-card/NFTMicroCard";
-import { useStyles } from "./CatalogStyles";
-import Link from "next/link";
-import { FRONT_CURRENT_PATH } from "../../config/env-paths";
+import {useStyles} from "./CatalogStyles";
 
 interface CatalogProps {
     nfts: NFTCard[];
@@ -18,11 +16,9 @@ const Catalog = ({nfts, layout, className}: CatalogProps) => {
         <div className={className}>
             <div className={classes[layout]}>
                 {nfts.map((item: NFTCard) => (
-                    <Link href={FRONT_CURRENT_PATH + ":3000/nft/" + item.nftCardId}>
-                        <div key={item.nftCardId} className={classes.card}>
-                            <NFTMicroCard item={item}/>
-                        </div>
-                    </Link>
+                    <div key={item.nftCardId} className={classes.card}>
+                        <NFTMicroCard item={item}/>
+                    </div>
                 ))}
             </div>
         </div>
--- front/src/components/nft-micro-card/NFTMicroCard.tsx
+++ front/src/components/nft-micro-card/NFTMicroCard.tsx
@@ -1,5 +1,5 @@
-import { NFTCard } from "../../schema/NFTCatalogSchema";
-import { useStyles } from "./NFTMicroCardStyles";
+import {NFTCard} from "../../schema/NFTCatalogSchema";
+import {useStyles} from "./NFTMicroCardStyles";
 import {
     GCBadge,
     GCGroup,
@@ -8,6 +8,8 @@ import {
     GCCardSection,
     GCImage,
 } from "../../gc-components";
+import {FRONT_CURRENT_PATH} from "../../config/env-paths";
+import Link from "next/link";
 
 interface NFTMicroCardProps {
     item?: NFTCard;
@@ -17,19 +19,21 @@ const NFTMicroCard = ({item}: NFTMicroCardProps) => {
     const {classes} = useStyles();
 
     return (
-        <GCCard withBorder radius="md" className={classes.card}>
-            <GCCardSection className={classes.imageSection}>
-                <GCImage src={item.nftImageUrl} alt="gun"/>
-            </GCCardSection>
-            <GCGroup className={classes.content} position="apart" mt="md">
-                <GCText weight={500}>{item.name}</GCText>
-                <GCBadge variant="gradient">{item.properties[0].name}</GCBadge>
-                <GCText size="sm" color="dimmed" weight={500} sx={{lineHeight: 2}} >
-                    Minting price
-                </GCText>
-                <GCText variant="gradient">{item.mintingPrice} ETH</GCText>
-            </GCGroup>
-        </GCCard>
+        <Link href={FRONT_CURRENT_PATH + ":3000/nft/" + item.nftCardId}>
+            <GCCard withBorder radius="md" className={classes.card}>
+                <GCCardSection className={classes.imageSection}>
+                    <GCImage src={item.nftImageUrl} alt="gun"/>
+                </GCCardSection>
+                <GCGroup className={classes.content} position="apart" mt="md">
+                    <GCText weight={500}>{item.name}</GCText>
+                    <GCBadge variant="gradient">{item.properties[0].name}</GCBadge>
+                    <GCText size="sm" color="dimmed" weight={500} sx={{lineHeight: 2}}>
+                        Minting price
+                    </GCText>
+                    <GCText variant="gradient">{item.mintingPrice} ETH</GCText>
+                </GCGroup>
+            </GCCard>
+        </Link>
     );
 };
 
