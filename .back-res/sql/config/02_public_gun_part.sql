b748aa4599c3d71218c741bdb234069ad33dfede
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-11-30 16:44:31 +0100

    33t9wwt: review fixes

--- .back-res/sql/config/02_public_gun_part.sql
+++ .back-res/sql/config/02_public_gun_part.sql
@@ -1,8 +1,8 @@
-INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, width, x, y, product_id, thumbnail_image) VALUES (5, 'qwe', 100, 100, 100, 5, 'qwe_thumb');
-INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, width, x, y, product_id, thumbnail_image) VALUES (3, 'qwe', 100, 100, 100, 3, 'qwe_thumb');
-INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, width, x, y, product_id, thumbnail_image) VALUES (1, 'qwe', 100, 100, 100, 1, 'qwe_thumb');
-INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, width, x, y, product_id, thumbnail_image) VALUES (4, 'qwe', 100, 100, 100, 4, 'qwe_thumb');
-INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, width, x, y, product_id, thumbnail_image) VALUES (2, 'qwe', 100, 100, 100, 2, 'qwe_thumb');
-INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, width, x, y, product_id, thumbnail_image) VALUES (6, 'qwe', 100, 100, 100, 6, 'qwe_thumb');
-INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, width, x, y, product_id, thumbnail_image) VALUES (7, 'qwe', 100, 100, 100, 7, 'qwe_thumb');
-INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, width, x, y, product_id, thumbnail_image) VALUES (8, 'qwe', 100, 100, 100, 8, 'qwe_thumb');
+INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, thumbnail_image, width, product_id) VALUES (1, 'qwe', 'qwe', 100, 1);
+INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, thumbnail_image, width, product_id) VALUES (2, 'qwe', 'qwe', 100, 2);
+INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, thumbnail_image, width, product_id) VALUES (3, 'qwe', 'qwe', 100, 3);
+INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, thumbnail_image, width, product_id) VALUES (4, 'qwe', 'qwe', 100, 4);
+INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, thumbnail_image, width, product_id) VALUES (5, 'qwe', 'qwe', 100, 5);
+INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, thumbnail_image, width, product_id) VALUES (6, 'qwe', 'qwe', 100, 6);
+INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, thumbnail_image, width, product_id) VALUES (7, 'qwe', 'qwe', 100, 7);
+INSERT INTO public.gun_part (gun_part_id, gun_part_image_url, thumbnail_image, width, product_id) VALUES (8, 'qwe', 'qwe', 100, 8);
