a0aac09dee1b6330536194507b14067354ee5267
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-12-01 20:42:14 +0100

    33z1hqt: review fixes

--- front/src/components/nft-card-information/NftCardInformation.tsx
+++ front/src/components/nft-card-information/NftCardInformation.tsx
@@ -1,66 +1,59 @@
-import { GCList, GCListItem, GCText } from "../../gc-components";
-import { NFTCard } from "../../schema/NFTCatalogSchema";
-import { useStyles } from "./NftCardInformationStyle";
-import { IconAlertCircle, IconPhoto } from "@tabler/icons";
+import {GCList, GCListItem, GCText} from "../../gc-components";
+import {NFTCard} from "../../schema/NFTCatalogSchema";
+import {useStyles} from "./NftCardInformationStyle";
+import {IconAlertCircle, IconPhoto} from "@tabler/icons";
 
 interface NftCardInformationProps {
-  data: NFTCard;
+    data: NFTCard;
 }
 
-const NftCardInformation = ({ data }: NftCardInformationProps) => {
-  const { classes } = useStyles();
+const NftCardInformation = ({data}: NftCardInformationProps) => {
+    const {classes} = useStyles();
 
-  return (
-    <div className={classes.infoContainer}>
-      <GCText h1 bold}>{data.name}</GCText>
-      <GCText weight={400} size={16} className={classes.collection}>
-        Collection:
-        <GCText className={classes.collectionName}>
-          {data.collection}
-        </GCText>
-      </GCText>
+    return (
+        <div className={classes.infoContainer}>
+            <GCText h1 bold> {data.name} </GCText>
+            <GCText weight={400} size={16} className={classes.collection}>
+                Collection:
+                <GCText className={classes.collectionName}>
+                    {data.collection}
+                </GCText>
+            </GCText>
 
-      <div className={classes.frame}>
-        <GCText size={16} weight={700} className={classes.frameTitle}>
-          <IconPhoto className={classes.frameIcon} /> About the Build
-        </GCText>
-        <GCText size={16} weight={400} className={classes.frameText}>
-          {data.gunDescription}
-        </GCText>
-      </div>
+            <div className={classes.frame}>
+                <GCText size={16} weight={700} className={classes.frameTitle}>
+                    <IconPhoto className={classes.frameIcon}/> About the Build
+                </GCText>
+                <GCText size={16} weight={400} className={classes.frameText}>
+                    {data.gunDescription}
+                </GCText>
+            </div>
 
-      <div className={classes.frame}>
-        <GCText size={16} weight={700} className={classes.frameTitle}>
-          <IconAlertCircle className={classes.frameIcon} /> More Info
-        </GCText>
-        <GCList size={16} listStyleType="none" className={classes.list}>
-          <GCListItem className={classes.listItem}>
-            <GCText>Minting Price</GCText>
-            <GCText weight={700} className={classes.listData}>{data.mintingPrice}</GCText>
-          </GCListItem>
-          <GCListItem className={classes.listItem}>
-            <GCText>Collection</GCText>
-            <GCText weight={700} className={classes.listData}>{data.collection}</GCText>
-          </GCListItem>
-          <GCListItem className={classes.listItem}>
-            <GCText>First Owner</GCText>
-            <GCText weight={700} className={classes.listData}>{data.firstOwner}</GCText>
-          </GCListItem>
-          <GCListItem className={classes.listItem}>
-            <GCText>Rarity</GCText>
-            <GCText weight={700} className={classes.listData}>{data.rarity}</GCText>
-          </GCListItem>
-          <GCListItem className={classes.listItem}>
-            <GCText>Creation Date</GCText>
-            <GCText weight={700} className={classes.listData}>{data.mintingDate}</GCText>
-          </GCListItem>
-        </GCList>
-      </div>
-      <div className={classes.textDescription}>Description:</div>
-
-      <GCText size={12}>{data.gunDescription}</GCText>
-    </div>
-  );
+            <div className={classes.frame}>
+                <GCText size={16} weight={700} className={classes.frameTitle}>
+                    <IconAlertCircle className={classes.frameIcon}/> More Info
+                </GCText>
+                <GCList size={16} listStyleType="none" className={classes.list}>
+                    <GCListItem className={classes.listItem}>
+                        <GCText>Minting Price</GCText>
+                        <GCText weight={700} className={classes.listData}>{data.mintingPrice}</GCText>
+                    </GCListItem>
+                    <GCListItem className={classes.listItem}>
+                        <GCText>First Owner</GCText>
+                        <GCText weight={700} className={classes.listData}>{data.firstOwner}</GCText>
+                    </GCListItem>
+                    <GCListItem className={classes.listItem}>
+                        <GCText>Rarity</GCText>
+                        <GCText weight={700} className={classes.listData}>{data.rarity}</GCText>
+                    </GCListItem>
+                    <GCListItem className={classes.listItem}>
+                        <GCText>Creation Date</GCText>
+                        <GCText weight={700} className={classes.listData}>{data.mintingDate}</GCText>
+                    </GCListItem>
+                </GCList>
+            </div>
+        </div>
+    );
 };
 
 export default NftCardInformation;
