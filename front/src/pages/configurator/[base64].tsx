42979540a61f5883965a9940eaa0208eba293df6
Strahinja Cokic <strahinjacokic@gmail.com>
2022-12-10 14:58:06 +0100

    Updated the branch

--- front/src/pages/configurator/[base64].tsx
+++ front/src/pages/configurator/[base64].tsx
@@ -3,13 +3,54 @@ import { useRouter } from "next/router";
 import { Engine } from "../../rendering-engine/components/engine";
 import { useGetBuildTreeByBase64Code } from "../../rendering-engine/service/configuratorService";
 import Link from "next/link";
-import {FRONT_CURRENT_PATH} from "../../config/env-paths";
+import { useEffect, useState } from "react";
+import { BuildTree } from "../../rendering-engine/schema/BuildTreeSchema";
 
 const Configurator = ({ testData }) => {
     const router = useRouter();
     const { base64 } = router.query;
 
     const [data] = useGetBuildTreeByBase64Code(base64 as string);
+    const [buildTree, setBuildTree] = useState<BuildTree | null>(null);
+
+    const newGrip = {
+        id: 20,
+        productId: 20,
+        name: "Timber Creek Enforcer Pistol Grip AR",
+        productImageUrl: "https://line-f.ru/i/part/1_MIJ4fpT.png",
+        description:
+            "The Enforcer AR Pistol Grip is a drop in replacement that mounts to any standard mil spec lower receiver. Weighing in at just over 4oz it does not add unnecessary weight to your rifle.",
+        brand: "Timber Creek",
+        type: "PISTOL_GRIP",
+        x: -5,
+        y: 54,
+        thumbnailImage: "https://line-f.ru/i/part/1_MIJ4fpT.png",
+        image: "https://line-f.ru/i/part/1_MIJ4fpT.png",
+        width: 486,
+        children: [],
+    };
+
+    const searchForItem = (buildTree, id, newItem) => {
+        let newBuildTree = { ...buildTree };
+
+        let newChildren = buildTree.children?.map((el) => {
+            let element = el.id === id ? newItem : el;
+            searchForItem(el, id, newItem);
+            return element;
+        });
+
+        newBuildTree.children = newChildren;
+        return newBuildTree;
+    };
+
+    const updateGrip = (id, newItem) => {
+        const newTree = searchForItem(buildTree, id, newItem);
+        setBuildTree(newTree);
+    };
+
+    useEffect(() => {
+        setBuildTree(data);
+    }, [data]);
 
     return (
         <MantineProvider withGlobalStyles withNormalizeCSS>
@@ -20,12 +61,19 @@ const Configurator = ({ testData }) => {
                     padding: "1rem",
                 }}
             >
-                {data && <Engine data={data} />}
+                {buildTree && <Engine data={buildTree} />}
             </Box>
-            <Link href={FRONT_CURRENT_PATH + ":3000/summary/" + base64}>
-                <button>To Summary Page</button>
-            </Link>
-        </MantineProvider>
+
+            <Center>
+                <Button onClick={() => updateGrip(10, newGrip)}>Update Grip</Button>
+            </Center>
+
+            <Center>
+                <Link href={{ pathname: "/summary/" + base64 }}>
+                    <Button>To Summary Page</Button>
+                </Link>
+            </Center>
+        </>
     );
 };
 
