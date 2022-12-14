7ca049e3a1ce7f7e940a9a2d1fda225d087e1090
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-12-14 14:55:49 +0100

    382rz54: review fixes

--- front/src/config/env-paths.ts
+++ front/src/config/env-paths.ts
@@ -1,5 +1,5 @@
 export const TEST = `http://178.128.203.194`;
 export const LOCAL = `http://localhost`;
 
-export const BACK_CURRENT_PATH = TEST;
-export const FRONT_CURRENT_PATH = TEST;
+export const BACK_CURRENT_PATH = LOCAL;
+export const FRONT_CURRENT_PATH = LOCAL;
