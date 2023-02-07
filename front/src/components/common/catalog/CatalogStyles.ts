2c12bbb87dd3f200bd4ab4e4e07ab39480e7371e
anastasiiakuzennaia <nastya.kuzennaya@gmail.com>
2023-02-07 12:16:28 +0000

    861m8y5fg-4: Catalog cards amount

--- front/src/components/common/catalog/CatalogStyles.ts
+++ front/src/components/common/catalog/CatalogStyles.ts
@@ -14,26 +14,38 @@ export const useStyles = createStyles((theme) => ({
     },
 
     catalog233: {
-        [`@media (min-width: ${theme.other.breakpoints.laptopS}px)`]: {
+        [`@media (min-width: ${theme.other.breakpoints.tablet}px)`]: {
             gridTemplateColumns: "1fr 1fr",
         },
         [`@media (min-width: ${theme.other.breakpoints.laptopM}px)`]: {
             gridTemplateColumns: "1fr 1fr 1fr",
         },
         [`@media (min-width: ${theme.other.breakpoints.laptopL}px)`]: {
-            gridTemplateColumns: "1fr 1fr 1fr",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+        },
+        [`@media (min-width: ${theme.other.breakpoints.laptopXL}px)`]: {
+            gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
+        },
+        [`@media (min-width: ${theme.other.breakpoints.laptopXXL}px)`]: {
+            gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
         },
     },
 
     catalog344: {
-        [`@media (min-width: ${theme.other.breakpoints.laptopS}px)`]: {
+        [`@media (min-width: ${theme.other.breakpoints.tablet}px)`]: {
             gridTemplateColumns: "1fr 1fr 1fr",
         },
         [`@media (min-width: ${theme.other.breakpoints.laptopM}px)`]: {
             gridTemplateColumns: "1fr 1fr 1fr 1fr",
         },
         [`@media (min-width: ${theme.other.breakpoints.laptopL}px)`]: {
-            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr",
+        },
+        [`@media (min-width: ${theme.other.breakpoints.laptopXL}px)`]: {
+            gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
+        },
+        [`@media (min-width: ${theme.other.breakpoints.laptopXXL}px)`]: {
+            gridTemplateColumns: "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr",
         },
     },
 }));
