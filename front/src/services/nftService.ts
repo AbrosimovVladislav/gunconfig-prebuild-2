17d8eb504d91431f5bb3aeff41c724d641c4bceb
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2023-02-24 23:50:31 +0100

    background: small fixes

--- front/src/services/nftService.ts
+++ front/src/services/nftService.ts
@@ -2,7 +2,8 @@ export function getCollectionName(gunName){
   // return "GunCraft-Initial";
   // return "GC-Initial-Orange";
   // return "GC-Initial-Mirror-Pink";
-  return "GC-Initial-Mirror-Extra-Pink";
+  // return "GC-Initial-Mirror-Extra-Pink";
+  return "GC-Initial-Metal-01";
 }
 
 export function getRarity(){
